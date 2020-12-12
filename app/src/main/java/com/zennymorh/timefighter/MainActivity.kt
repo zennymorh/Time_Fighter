fd36a2f055a656d55b43e561dc57e42708f4ea69
Zainab Jimoh <jimohzainab4753@gmail.com>
2020-12-12 13:38:49 +0100

    Implemented the functionality of the timer and tap action

--- app/src/main/java/com/zennymorh/timefighter/MainActivity.kt
+++ app/src/main/java/com/zennymorh/timefighter/MainActivity.kt
@@ -3,6 +3,7 @@ package com.zennymorh.timefighter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.util.Log
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -11,13 +12,21 @@ class MainActivity : AppCompatActivity() {
     private var score = 0
     private var gameStarted = false
 
-    internal lateinit var countDownTimer: CountDownTimer
+    private lateinit var countDownTimer: CountDownTimer
     internal val initialCountDown: Long = 60000
     internal val countDownInterval: Long = 1000
+    internal var timeLeftOnTimer: Long = 60000
+
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
+        private const val SCORE_KEY = "SCORE_KEY"
+        private const val TIME_LEFT_KEY = "TIME_LEFT_KEY"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate called. Score: $score")
 
         tapMeButton.setOnClickListener {
             incrementScore()
@@ -54,6 +63,7 @@ class MainActivity : AppCompatActivity() {
 
         countDownTimer = object : CountDownTimer(initialCountDown, countDownInterval) {
             override fun onTick(millisUntilFinished: Long) {
+                timeLeftOnTimer = millisUntilFinished
                 val timeLeft = millisUntilFinished/1000
                 timeLeftTextView.text = getString(R.string.timeLeft, timeLeft)
             }
