275d68065bb0297471c7246e67ce6d26f9d20f97
Zainab Jimoh <jimohzainab4753@gmail.com>
2020-12-14 04:17:04 +0100

    Added a menu to the app

--- app/src/main/java/com/zennymorh/timefighter/MainActivity.kt
+++ app/src/main/java/com/zennymorh/timefighter/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.view.animation.AnimationUtils
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
@@ -42,6 +44,22 @@ class MainActivity : AppCompatActivity() {
         else {
             resetGame()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.actionAbout) {
+            showInfo()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun showInfo() {
+
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
